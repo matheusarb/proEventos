21d8f51cde5bc960522c5b254996ecf3d99cd82d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-01 18:56:39 -0300

    feat: addding Update method

--- back/src/ProEventos.Domain/Evento.cs
+++ back/src/ProEventos.Domain/Evento.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProEventos.Domain.Extensions;
 
 namespace ProEventos.Domain
 {
@@ -20,5 +21,22 @@ namespace ProEventos.Domain
         public IEnumerable<Lote> Lotes { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
         public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+
+        public Evento Update(Evento model)
+        {
+            Id = Id;
+            Local = model.Local.IsNullOrEmptyOrWhiteSpace() ? Local : model.Local;
+            Data = model.Data == null ? Data : model.Data;
+            Tema = model.Tema.IsNullOrEmptyOrWhiteSpace() ? Tema : model.Tema;
+            QtdPessoas = model.QtdPessoas == 0 ? QtdPessoas : model.QtdPessoas;
+            ImagemURL = model.ImagemURL.IsNullOrEmptyOrWhiteSpace() ? ImagemURL : model.ImagemURL;
+            Telefone = model.Telefone.IsNullOrEmptyOrWhiteSpace() ? Telefone : model.Telefone;
+            Email = model.Email.IsNullOrEmptyOrWhiteSpace() ? Email : model.Email;
+            Lotes = model.Lotes == null || !model.Lotes.Any() ? Lotes : model.Lotes;
+            RedesSociais = model.RedesSociais == null || !model.RedesSociais.Any() ? RedesSociais : model.RedesSociais;
+            PalestrantesEventos = model.PalestrantesEventos == null || !model.PalestrantesEventos.Any() ? PalestrantesEventos : model.PalestrantesEventos;
+
+            return this;
+        }
     }
 }
