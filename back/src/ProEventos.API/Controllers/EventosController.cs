11f6ed5a0702b50f0d9cf7e43e2a854080e47bf7
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-05-01 00:03:37 -0300

    feat: adding endpoints into EventosController

--- back/src/ProEventos.API/Controllers/EventosController.cs
+++ back/src/ProEventos.API/Controllers/EventosController.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ProEventos.API.Data;
+using ProEventos.Persistence;
+using ProEventos.Persistence.Contracts;
 using ProEventos.Domain;
+using ProEventos.Application;
+using ProEventos.Application.Contracts;
+using Microsoft.AspNetCore.Http;
 
 namespace ProEventos.API.Controllers
 {
@@ -13,25 +17,80 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IEventoService _eventoService;
+        private readonly IGeralPersist _geralPersist;
 
-        public EventosController(DataContext context)
+        public EventosController(IEventoService eventoService)
         { 
-            _context = context;
+            _eventoService = eventoService;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Evento>> GetEventos()
+        public async Task<IActionResult> GetAllEventosAsync()
         {
-            var eventos = await _context.Eventos.AsNoTracking().ToListAsync();
-            return eventos;
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync();
+                if(eventos == null)
+                    return NotFound("Não foi possível completar a requisição");
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
         }
 
         [HttpGet("{id:int}")]
-        public async Task<Evento> GetById(int id)
+        public async Task<IActionResult> GetByIdAsync(int id)
         {
-            var evento = await _context.Eventos.FirstOrDefaultAsync(ev => ev.Id == id);
-            return evento;
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id);
+                if(evento==null)
+                    return NotFound("Evento não encontrado.");
+                return Ok(evento);                
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar evento. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{tema}")]
+        public async Task<IActionResult> GetByTemaAsync(string tema)
+        {
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosByTemaAsync(tema);
+                if(eventos.Count == 0)
+                    return NotFound("Eventos por tema não encontrados com o parâmetro fornecido.");
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+    
+        [HttpPost]
+        public async Task<IActionResult> Post(Evento model)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                    return BadRequest("Não foi possível criar o evento.");
+                
+                var evento = await _eventoService.AddEvento(model);
+                if(evento == null)
+                    return BadRequest("Erro ao criar o evento.");
+                
+                return Created($"/{evento.Id}", evento);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao criar o evento. Erro: {ex.Message}");
+            }
         }
     }
 }
--- back/src/ProEventos.Domain/Evento.cs
+++ back/src/ProEventos.Domain/Evento.cs
@@ -7,6 +7,7 @@ namespace ProEventos.Domain
 {
     public class Evento
     {
+        
         public int Id { get; set; }
         public string Local { get; set; }
         public DateTime? Data { get; set; }
